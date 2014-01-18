f9e022ef0ff0a840d9a8cfb023dcda25c1341b43
Andrew Cole <andrew.cole@illallangi.com>
2014-01-18 20:05:11 +1100

    Added INinjectSettings constructor argument; Removed mutiple type implementations

--- src/Illallangi.NinjectCmdlet/NinjectCmdlet.cs
+++ src/Illallangi.NinjectCmdlet/NinjectCmdlet.cs
@@ -4,27 +4,46 @@ using Ninject.Modules;
 
 namespace Illallangi
 {
-    [Cmdlet(VerbsCommon.Get, NinjectCmdlet<T1>.Null)]
-    public abstract class NinjectCmdlet<T1> : PSCmdlet
-        where T1 : INinjectModule, new()
+    [Cmdlet(VerbsCommon.Get, NinjectCmdlet<T>.Null)]
+    public abstract class NinjectCmdlet<T> : PSCmdlet
+        where T : INinjectModule, new()
     {
         #region Fields
 
         protected const string Null = @"Null";
 
-        private INinjectModule currentModule1;
+        private readonly INinjectSettings currentNinjectSettings;
+
+        private INinjectModule currentModule;
 
         private StandardKernel currentKernel;
 
         #endregion
 
+        #region Constructor
+
+        protected NinjectCmdlet(INinjectSettings ninjectSettings = null)
+        {
+            this.currentNinjectSettings = ninjectSettings ?? new NinjectSettings();
+        }
+
+        #endregion
+
         #region Properties
 
-        private INinjectModule Module1
+        private INinjectSettings NinjectSettings
         {
             get
             {
-                return this.currentModule1 ?? (this.currentModule1 = new T1());
+                return this.currentNinjectSettings;
+            }
+        }
+
+        private INinjectModule Module
+        {
+            get
+            {
+                return this.currentModule ?? (this.currentModule = new T());
             }
         }
 
@@ -32,7 +51,8 @@ namespace Illallangi
         {
             get
             {
-                return this.currentKernel ?? (this.currentKernel = new StandardKernel(this.Module1));
+                return this.currentKernel
+                       ?? (this.currentKernel = new StandardKernel(this.NinjectSettings, this.Module));
             }
         }
 
@@ -47,128 +67,4 @@ namespace Illallangi
 
         #endregion
     }
-
-    [Cmdlet(VerbsCommon.Get, NinjectCmdlet<T1, T2>.Null)]
-    public abstract class NinjectCmdlet<T1, T2> : PSCmdlet
-        where T1 : INinjectModule, new()
-        where T2 : INinjectModule, new()
-    {
-        #region Fields
-
-        protected const string Null = @"Null";
-
-        private INinjectModule currentModule1;
-
-        private INinjectModule currentModule2;
-
-        private StandardKernel currentKernel;
-
-        #endregion
-
-        #region Properties
-
-        private INinjectModule Module1
-        {
-            get
-            {
-                return this.currentModule1 ?? (this.currentModule1 = new T1());
-            }
-        }
-
-        private INinjectModule Module2
-        {
-            get
-            {
-                return this.currentModule2 ?? (this.currentModule2 = new T2());
-            }
-        }
-
-        private StandardKernel Kernel
-        {
-            get
-            {
-                return this.currentKernel ?? (this.currentKernel = new StandardKernel(this.Module1, this.Module2));
-            }
-        }
-
-        #endregion
-
-        #region Methods
-
-        protected T Get<T>()
-        {
-            return this.Kernel.Get<T>();
-        }
-
-        #endregion
-    }
-
-
-    [Cmdlet(VerbsCommon.Get, NinjectCmdlet<T1, T2, T3>.Null)]
-    public abstract class NinjectCmdlet<T1, T2, T3> : PSCmdlet
-        where T1 : INinjectModule, new()
-        where T2 : INinjectModule, new()
-        where T3 : INinjectModule, new()
-    {
-        #region Fields
-
-        protected const string Null = @"Null";
-
-        private INinjectModule currentModule1;
-
-        private INinjectModule currentModule2;
-
-        private INinjectModule currentModule3;
-
-        private StandardKernel currentKernel;
-
-        #endregion
-
-        #region Properties
-
-        private INinjectModule Module1
-        {
-            get
-            {
-                return this.currentModule1 ?? (this.currentModule1 = new T1());
-            }
-        }
-
-        private INinjectModule Module2
-        {
-            get
-            {
-                return this.currentModule2 ?? (this.currentModule2 = new T2());
-            }
-        }
-
-        private INinjectModule Module3
-        {
-            get
-            {
-                return this.currentModule3 ?? (this.currentModule3 = new T3());
-            }
-        }
-
-        private StandardKernel Kernel
-        {
-            get
-            {
-                return this.currentKernel ?? (this.currentKernel = new StandardKernel(this.Module1, this.Module2, this.Module3));
-            }
-        }
-
-        #endregion
-
-        #region Methods
-
-        protected T Get<T>()
-        {
-            return this.Kernel.Get<T>();
-        }
-
-        #endregion
-    }
-
-
 }
