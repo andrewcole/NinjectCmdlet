e3d5375fc070e6d2c111bbf9135e89c340a2b664
Andrew Cole <andrew.cole@illallangi.com>
2014-03-05 19:56:57 +1100

    Added ModuleConstructor argument to constructor

--- src/Illallangi.NinjectCmdlet/NinjectCmdlet.cs
+++ src/Illallangi.NinjectCmdlet/NinjectCmdlet.cs
@@ -1,18 +1,22 @@
-﻿using System.Management.Automation;
+﻿using System;
+using System.Management.Automation;
+
 using Ninject;
 using Ninject.Modules;
 
 namespace Illallangi
 {
-    [Cmdlet(VerbsCommon.Get, NinjectCmdlet<T>.Null)]
-    public abstract class NinjectCmdlet<T> : PSCmdlet
-        where T : INinjectModule, new()
+    [Cmdlet(VerbsCommon.Get, NinjectCmdlet<Tmodule>.Null)]
+    public abstract class NinjectCmdlet<Tmodule> : PSCmdlet
+        where Tmodule : INinjectModule, new()
     {
         #region Fields
 
         protected const string Null = @"Null";
 
         private readonly INinjectSettings currentNinjectSettings;
+
+        private readonly Func<Tmodule> currentModuleConstructor;
 
         private INinjectModule currentModule;
 
@@ -22,9 +26,10 @@ namespace Illallangi
 
         #region Constructor
 
-        protected NinjectCmdlet(INinjectSettings ninjectSettings = null)
+        protected NinjectCmdlet(INinjectSettings ninjectSettings = null, Func<Tmodule> moduleConstructor = null)
         {
             this.currentNinjectSettings = ninjectSettings ?? new NinjectSettings();
+            this.currentModuleConstructor = moduleConstructor;
         }
 
         #endregion
@@ -39,11 +44,22 @@ namespace Illallangi
             }
         }
 
+        private Func<Tmodule> ModuleConstructor
+        {
+            get
+            {
+                return this.currentModuleConstructor;
+            }
+        }
+
         private INinjectModule Module
         {
             get
             {
-                return this.currentModule ?? (this.currentModule = new T());
+                return this.currentModule ?? 
+                    (this.currentModule = (null == this.ModuleConstructor) ? 
+                                            this.ModuleConstructor() : 
+                                            new Tmodule());
             }
         }
 
